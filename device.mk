b2b5cfea2d068d78f38a8053d9b86a08131ddafb
LuK1337 <priv.luk@gmail.com>
2023-12-22 19:59:44 +0100

    FP5: Update WFD system stack from OOS 14.0.0.81(EX01)
    Change-Id: Ica1450a1568fe9c7eb8087ed0fa567493150af5d

--- device.mk
+++ device.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
     init.qti.display_boot.sh \
     init.qti.display_boot.rc \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     libqdMetaData \
     libsdmcore \
     libsdmutils \
@@ -159,6 +160,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.config-V5-ndk \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
