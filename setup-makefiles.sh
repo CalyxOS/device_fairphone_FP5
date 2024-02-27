cb0f79b4447b1933e849e1f76a7a0cdc966cbafe
Michael Bestas <mkbestas@gmail.com>
2024-02-27 23:17:00 +0200

    Add makefile entries for factory partitions
    Change-Id: Iaf56c47698cd772f553f7054f049de9614f48ca6

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,5 +34,14 @@ write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
 
+printf '%s\n' "\$(call add-radio-file,factory/apdp.img)" >> "${ANDROIDMK}"
+printf '%s\n' "\$(call add-radio-file,factory/ddr.img)" >> "${ANDROIDMK}"
+printf '%s\n' "\$(call add-radio-file,factory/frp.img)" >> "${ANDROIDMK}"
+printf '%s\n' "\$(call add-radio-file,factory/logfs.img)" >> "${ANDROIDMK}"
+printf '%s\n' "\$(call add-radio-file,factory/rtice.img)" >> "${ANDROIDMK}"
+printf '%s\n' "\$(call add-radio-file,factory/storsec.img)" >> "${ANDROIDMK}"
+printf '%s\n' "\$(call add-radio-file,factory/study.img)" >> "${ANDROIDMK}"
+printf '\n' >> "${ANDROIDMK}"
+
 # Finish
 write_footers
