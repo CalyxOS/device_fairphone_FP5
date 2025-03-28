114203edfaa4db5e0af36214263d884f047f5039
Michael Bestas <mkbestas@gmail.com>
2025-03-28 16:36:02 +0200

    calyx: Bump to FP5.UT2L.B.107.20250218
    Change-Id: I66a1899abe6dce9612ecb18b8cf6ef85b53add6f

--- calyx_FP5.mk
+++ calyx_FP5.mk
@@ -22,5 +22,5 @@ PRODUCT_MODEL := FP5
 PRODUCT_MANUFACTURER := Fairphone
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Fairphone/FP5/FP5:14/UKQ1.230924.001/UT2K:user/release-keys \
+    BuildFingerprint=Fairphone/FP5/FP5:14/UKQ1.230924.001/UT2L:user/release-keys \
     DeviceProduct=FP5
